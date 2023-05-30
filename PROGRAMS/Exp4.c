c4a77c74f1a9552a3f226a8a920c5b2984a72605
Adwait S Purao <adwait.purao@spit.ac.in>
2023-05-30 22:12:49 +0530

    Update Exp4.c

--- PROGRAMS/Exp4.c
+++ PROGRAMS/Exp4.c
@@ -1,17 +1,35 @@
+## Code:
+### Problem 1:
+### `Factorial`
+```c
 #include <stdio.h>
-#include<stdlib.h>
 
 long int factorial(int n)
 {
     if (n <= 1)
     {
-        return (1);
+        return 1;
     }
     else
     {
-        return (n * factorial(n - 1));
+        return n * factorial(n - 1);
     }
 }
+
+int main()
+{
+    int n1;
+    printf("Enter a value of n=");
+    scanf("%d", &n1);
+    printf("n! = %ld ", factorial(n1));
+
+    return 0;
+}
+```
+### Problem 2:
+### `Fibonacci Series`
+```c
+#include <stdio.h>
 
 long int fib(int count)
 {
@@ -25,9 +43,28 @@ long int fib(int count)
     }
     else
     {
-        return (fib(count - 1) + fib(count - 2));
+        return fib(count - 1) + fib(count - 2);
     }
 }
+
+int main()
+{
+    int n, count;
+    printf("Enter the value of n=");
+    scanf("%d", &n);
+
+    for (count = 1; count <= n; ++count)
+    {
+        printf("i=%2d    fibonacci number=%ld \n", count, fib(count));
+    }
+
+    return 0;
+}
+```
+### Problem 3:
+### `Series`
+```c
+#include <stdio.h>
 
 int series(int n)
 {
@@ -45,6 +82,21 @@ int series(int n)
         return num;
     }
 }
+
+int main()
+{
+    int n3;
+    printf("Enter a number:\n");
+    scanf("%d", &n3);
+    series(n3);
+
+    return 0;
+}
+```
+### Problem 4:
+### `Ackermann Function`
+```c
+#include <stdio.h>
 
 int ack(int m, int n)
 {
@@ -73,7 +125,6 @@ void table()
         while (m <= 3)
         {
             printf("%d | %d | %d\n", m, n, ack(m, n));
-
             m++;
         }
         m = 1;
@@ -81,13 +132,24 @@ void table()
     }
 }
 
+int main()
+{
+    table();
+    return 0;
+}
+```
+### Problem 5:
+### `Recaman Series`
+```c
+#include <stdio.h>
+
 int rec(int n)
 {
     if (n == 1)
     {
         return 1;
     }
-    else if ((rec(n - 1) - n) > 0)
+    else if (rec(n - 1) - n > 0)
     {
         return rec(n - 1) - n;
     }
@@ -99,36 +161,10 @@ int rec(int n)
 
 int main()
 {
-    // Problem 1
-    int n1;
-    printf("Enter a value of n=");
-    scanf("%d", &n1);
-    printf("n! = %ld ", factorial(n1));
-
-    // Problem 2
-    int n, count, i;
-    printf("Enter the value of n=");
-    scanf("%d", &n);
-
-    for (count = 1; count <= n; ++count)
-    {
-        printf("i=%2d    fibonacci number=%ld \n", count, fib(count));
-    }
-
-    // Problem 3
-    int i, n3;
-    printf("Enter a number:\n");
-    scanf("%d", &n3);
-    series(n3);
-
-    // Problem 4
-    table();
-
-    // Problem 5
     int n2;
     printf("Enter a number: ");
     scanf("%d", &n2);
-    printf("Recaman series upto the %dth term is:\n ", n2);
+    printf("Recaman series up to the %dth term is:\n ", n2);
     for (int i = 1; i <= n2; i++)
     {
         printf("%d ", rec(i));
@@ -136,3 +172,4 @@ int main()
 
     return 0;
 }
+```
